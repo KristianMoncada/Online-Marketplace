859e666f812821c700bb31b3ecd1a17471bc22d5
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2024-08-01 19:51:58 -0500

    item details
    Added item details

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -18,12 +18,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('items.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
     
