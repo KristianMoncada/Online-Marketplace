a593190aef7950348a47af4f7212e493258b1709
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2024-08-02 19:02:47 -0500

    Add Item
    Started the add item

--- puddle/items/urls.py
+++ puddle/items/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+
 app_name = 'item'
 
 urlpatterns = [
+    path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
 ]
