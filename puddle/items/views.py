d90a1c54aa11e115c27b033736dec6cdbc36c1d7
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2024-08-08 15:37:18 -0400

    Edit items
    Added ability to edit items

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
+
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
@@ -21,6 +24,29 @@ def delete(request, pk):
     return redirect('dashboard:index')
 
 @login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+    
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.id)
+        else:
+            print(form.errors)  # Print errors to console for debugging
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+        'item': item,  # Ensure item is included in context
+    })
+
+
+
+@login_required
 def new(request):
     if request.method == 'POST':
         form = NewItemForm(request.POST, request.FILES)
@@ -29,14 +55,13 @@ def new(request):
             item = form.save(commit=False)
             item.created_by = request.user
             item.save()
-
-            return redirect('item:detal', pk=item.id)
+            return redirect('item:detail', pk=item.id)
     else:
         form = NewItemForm()
 
     return render(request, 'item/form.html', {
-        'form' : form,
-        'title' : 'New Item',
+        'form': form,
+        'title': 'New Item',
     })
 
 @login_required
